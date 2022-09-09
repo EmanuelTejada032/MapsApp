f9429bb2cebe06cc6ea8b7b3fd0b81c63653c299
Emanuel De Jesús Tejada Coste <Emanuel De Jess Tejada Coste@sisalril.gov.do>
2022-09-09 11:07:44 -0400

    Add simple HttpClient to request data from mapbox api url, basic start

--- MapsApp/src/app/maps/components/search-bar/search-bar.component.ts
+++ MapsApp/src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
+
 export class SearchBarComponent  {
   private debounceTimer?: NodeJS.Timeout;
+
+  constructor(private placesServices: PlacesService){}
   
   onQueryChanged(query: string = ''){
     if(this.debounceTimer) clearTimeout(this.debounceTimer);
     this.debounceTimer = setTimeout( () => {
-      console.log("Query sent");
+      this.placesServices.getPlacesByQuery(query)
     },500)
   }
+  
 }
--- MapsApp/src/app/maps/services/places.service.ts
+++ MapsApp/src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
@@ -6,13 +8,16 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
   public useLocation?: [number, number] 
 
+  placesLoading: boolean = false;
+  places: Feature[] = [];
+
   
   
   get isUseLocationReady():boolean {
     return !!this.useLocation;
   }
 
-  constructor() { 
+  constructor(private http: HttpClient) { 
     this.getUseLocation();
   }
 
@@ -25,10 +30,20 @@ export class PlacesService {
         },
         (err) => {
           console.log(err);
-          console.log("Location couldnt be found");
+          console.log('Location couldnt be found');
           reject();
         }
       )
+    })
+  }
+
+  public getPlacesByQuery(query: string = ''){
+    this.placesLoading = true;
+    return this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?country=do&limit=10&proximity=-69.93190757074991%2C18.465133862176017&types=place%2Caddress%2Cneighborhood&language=es&access_token=pk.eyJ1IjoiaGVybWFuZGFkMDMyIiwiYSI6ImNreGhpMmcydDBhdmcycHF3MHJ0MnJtZHAifQ.dxim_rQxF48qe2pYPVovyA`)
+    .subscribe( resp => {
+      this.placesLoading = false;
+      this.places = resp.features;
+      console.log(this.places);
     })
   }
 }
