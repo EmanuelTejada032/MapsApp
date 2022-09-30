3b24f9571884fcd2f963b89a2b25f72dfeab5d17
Emanuel De Jesús Tejada Coste <e.coste@sisalril.gob.do>
2022-09-30 10:08:21 -0400

    Display search result, fly to clicked location, apply selected style

--- MapsApp/src/app/maps/components/search-results/search-results.component.ts
+++ MapsApp/src/app/maps/components/search-results/search-results.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../../services/places.service';
+import { Feature } from '../../interfaces/places';
+import { MapService } from '../../services';
 
 @Component({
   selector: 'app-search-results',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultsComponent implements OnInit {
 
-  constructor() { }
+  selectedId: string = '0';
+
+  get placesLoading(): boolean{return this.placesServices.placesLoading};
+  get places(): Feature[]{return this.placesServices.places};
+
+  constructor(private placesServices: PlacesService
+              , private mapService: MapService) { }
 
   ngOnInit(): void {
+  }
+
+
+  flyTo(place: Feature){
+    this.selectedId = place.id;
+    const [lng, lat] = place.center;
+    this.mapService.flyTo([lng, lat])
   }
 
 }
